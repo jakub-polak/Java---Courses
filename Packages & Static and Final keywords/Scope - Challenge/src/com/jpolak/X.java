646b25495929047da3b9fa848d65351cc4f95dff
polak95 <jakubpolak95@gmail.com>
2018-12-01 11:51:30 +0100

    Update and Add Scope Challenge

---
+++ Packages & Static and Final keywords/Scope - Challenge/src/com/jpolak/X.java
@@ -0,0 +1,18 @@
+package com.jpolak;
+
+import java.util.Scanner;
+
+public class X {
+    private int x;
+
+    public X(int x) {
+        this.x = x;
+    }
+
+    public void x() {
+        for (int x = 0; x <= this.x; x++) {
+            System.out.println(this.x + " x " + x + " = " + this.x * x);
+        }
+    }
+
+}
