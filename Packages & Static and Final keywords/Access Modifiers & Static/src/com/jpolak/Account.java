4f81df02e8c04f670986bea7024e74f3b71f6582
polak95 <jakubpolak95@gmail.com>
2018-12-03 14:48:04 +0100

    Update progress

---
+++ Packages & Static and Final keywords/Access Modifiers & Static/src/com/jpolak/Account.java
@@ -0,0 +1,48 @@
+package com.jpolak;
+
+import java.util.ArrayList;
+
+public class Account {
+    private String accountName;
+    private static int balance = 0;
+    private ArrayList<Integer> transactions;
+
+    public Account(String accountName) {
+        this.accountName = accountName;
+        this.transactions = new ArrayList<Integer>();
+    }
+
+    public static int getBalance() {
+        System.out.println("Checking the balance...");
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        if(amount > 0) {
+            transactions.add(amount);
+            this.balance += amount;
+            System.out.println(amount + " deposited. Balance is now " + this.balance);
+        } else {
+            System.out.println("Nothing to deposite.");
+        }
+    }
+
+    public void withdraw(int amount) {
+        int withdraw = -amount;
+        if (withdraw < 0) {
+            this.transactions.add(withdraw);
+            this.balance += withdraw;
+            System.out.println(amount + " withdrawn. Balance is now " + this.balance);
+        } else {
+            System.out.println("Can not withdraw");
+        }
+    }
+
+    public void calculateBalance() {
+        this.balance = 0;
+        for(int i: this.transactions) {
+            this.balance += i;
+        }
+        System.out.println("Calculated balance is " + this.balance);
+    }
+}
---
+++ Packages & Static and Final keywords/Access Modifiers & Static/src/com/jpolak/Main.java
@@ -0,0 +1,17 @@
+package com.jpolak;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   /* Account timsAccount = new Account("Tim");
+	    timsAccount.deposit(1000);
+        timsAccount.withdraw(300);
+        timsAccount.withdraw(-200);
+
+        System.out.println("Balance: " + timsAccount.getBalance());*/
+
+        Account.getBalance();
+
+
+    }
+}
