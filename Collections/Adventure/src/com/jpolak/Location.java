7c204e1fa046f5b970a27a525945ed5635d9efc6
polak95 <jakubpolak95@gmail.com>
2018-12-05 19:34:41 +0100

    Update and Add Sets & HashSets

--- Collections/Adventure/src/com/jpolak/Location.java
+++ Collections/Adventure/src/com/jpolak/Location.java
@@ -2,22 +2,31 @@ package com.jpolak;
 
 import java.util.HashMap;
 import java.util.Map;
+//Here is your challenge and the challenges to work out
+//what is wrong with the location constructor that would
+//allow the program to compile and crash at runtime so
+//when you identify the problem modify the code to fix it
 
 public class Location {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        if (exits != null) {
+            this.exits = new HashMap<String, Integer>(exits);
+        } else {
+            this.exits = new HashMap<String, Integer>();
+        }
+
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
+/*    public void addExit(String direction, int location) {
         exits.put(direction, location);
-    }
+    }*/
 
     public int getLocationID() {
         return locationID;
